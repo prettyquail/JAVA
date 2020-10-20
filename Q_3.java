ab2668e29649dd60b52ba4e396f08b6a552e248a
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-20 21:45:02 +0530

    Create Q_3.java

---
+++ Q_3.java
@@ -0,0 +1,16 @@
+public class Selection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		int y=5;
+		if(x<y) {
+			System.out.println("X is less than Y");
+		}else if(x>y){
+			System.out.println("X is greater than Y");
+		}else {
+			System.out.println("X is equal to Y");
+		}
+	}
+
+}
