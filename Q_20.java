875457b315e36fc3152ff8ccf85f51c9d509ac6f
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-22 00:28:59 +0530

    Create Q_20.java

---
+++ Q_20.java
@@ -0,0 +1,16 @@
+package New;
+
+public class Q_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String theString = "This is a good day to code";
+        for(int i=theString.length()-1;i>=0;i--) {
+		System.out.print( theString.charAt(i) );
+        }
+	}
+
+}
+/*Output
+edoc ot yad doog a si sihT
+*/
