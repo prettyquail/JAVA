f11676e248f939181e35e72b1cea0997dd21a907
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-22 00:23:58 +0530

    Create Q_19.java

---
+++ Q_19.java
@@ -0,0 +1,16 @@
+package New;
+
+public class Q_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "Hello World";
+		int length = string.length();
+		System.out.println("Length="+length);
+
+	}
+
+}
+/*Output
+Length=11
+*/
