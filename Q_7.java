946bdeaad3423216eba2ad27becd01ac3bdb86b5
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-21 18:59:15 +0530

    Create Q_7.java

---
+++ Q_7.java
@@ -0,0 +1,28 @@
+package New;
+
+public class Q_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Number\t\tSqaure\t\tCube" );
+		for(int i=1;i<=10;i++) {
+			System.out.println(i+"\t\t "+i*i+"\t\t "+i*i*i+"\t\t ");
+		}
+
+	}
+
+}
+
+/*Output
+Number		Sqaure		Cube
+1		       1		      1		 
+2		       4		      8		 
+3		       9		     27		 
+4		       16		      64		 
+5		       25		      125		 
+6		       36		       216		 
+7		       49		      343		 
+8		       64		      512		 
+9		       81		      729		 
+10		     100		    1000		 
+*/
