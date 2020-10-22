8c158965863193e58bf3e4aef4bc04f857b9f3c0
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-22 09:01:48 +0530

    Create Q_22.java

---
+++ Q_22.java
@@ -0,0 +1,20 @@
+package New;
+
+public class Q_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String theString = "This IS a mix of UPPERcase and lowerCASE";
+
+		String uppercase = theString.toUpperCase();
+		String lowercase = theString.toLowerCase();
+		System.out.println("Uppercase string="+uppercase);
+		System.out.println("Lowercase string="+lowercase);
+
+	}
+
+}
+/*Output
+Uppercase string=THIS IS A MIX OF UPPERCASE AND LOWERCASE
+Lowercase string=this is a mix of uppercase and lowercase
+*/
