da8375fc0e963d09dcdc1d95cb7ce5df56110e0d
Manisha Sahu <manishasahu1903@gmail.com>
2020-10-22 09:26:11 +0530

    Create Q_23.java

---
+++ Q_23.java
@@ -0,0 +1,12 @@
+package New;
+
+public class Employee {
+    
+	String firstname,lastname;
+	double salary;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
