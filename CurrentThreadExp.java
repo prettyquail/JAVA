2f6e16fa9e26473c4de9309320f265a032d78a27
Manisha Sahu <manishasahu1903@gmail.com>
2020-11-16 14:29:27 +0530

    Create CurrentThreadExp.java

---
+++ CurrentThreadExp.java
@@ -0,0 +1,17 @@
+public class CurrentThreadExp extends Thread  
+{    
+    public void run()  
+    {    
+        // print currently executing thread   
+        System.out.println(Thread.currentThread().getName());    
+    }    
+    public static void main(String args[])  
+    {    
+        // creating two thread  
+        CurrentThreadExp t1=new CurrentThreadExp();    
+        CurrentThreadExp t2=new CurrentThreadExp();    
+        // this will call the run() method  
+        t1.start();    
+        t2.start();    
+    }    
+}  
