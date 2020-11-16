2d3a7af3b888592db679c286a8e50afbaecfa898
Manisha Sahu <manishasahu1903@gmail.com>
2020-11-16 13:09:40 +0530

    Create Q_51.java

---
+++ Q_51.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Q_51{
+	public static void main(String args[]){
+		String[] s = { "sort", "string", "array" };
+		Arrays.sort(s);
+		System.out.println(Arrays.toString(s));
+
+	}
+}
