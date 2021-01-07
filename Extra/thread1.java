6a0db5ede75a57e244c8c1725a1e6a1c71d7ba98
Manisha Sahu <manishasahu1903@gmail.com>
2021-01-07 12:02:56 +0530

    Create thread1.java
    Thread example 1
    07-01-2021

---
+++ Extra/thread1.java
@@ -0,0 +1,19 @@
+class thread1{
+	public static void main(String args[]){
+		Thread t=Thread.currentThread();
+		System.out.println("Current Thread"+t);
+		t.setName("MyThread");
+		System.out.println("After name change"+t);
+
+		try{
+			for(int n=5;n>0;n--){
+				System.out.println(n);
+				Thread.sleep(1000); // t.sleep(1000)
+			}
+		}
+		catch(InterruptedException e){
+			System.out.println("Main Thread Interrupted");
+		}
+
+	}
+}
